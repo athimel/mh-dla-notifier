805a3eb1702de126ce263ab002581a707c7c496f
No author <no-author@xyz.com>
2012-03-01 14:34:29 +0000

    Anomalie #81 Calcul du MD5 erroné

--- src/org/zoumbox/mh_dla_notifier/MhDlaNotifierUtils.java
+++ src/org/zoumbox/mh_dla_notifier/MhDlaNotifierUtils.java
@@ -2,6 +2,7 @@ package org.zoumbox.mh_dla_notifier;
 
 import android.util.Log;
 import com.google.common.base.Strings;
+import org.apache.commons.codec.binary.Hex;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -13,6 +14,20 @@ public class MhDlaNotifierUtils {
 
     private static final String TAG = "MhDlaNotifier-" + MhDlaNotifierUtils.class.getSimpleName();
 
+    /**
+     * Encodes a raw byte[] to an hexadecimal String
+     *
+     * @param raw the byte[] to encode
+     * @return the hexadecimal encoded text
+     */
+    public static String toHexadecimal(byte[] raw) {
+        String result = null;
+        if (raw != null) {
+            result = new String(Hex.encodeHex(raw));
+        }
+        return result;
+    }
+
     public static String md5(String text) {
         if (!Strings.isNullOrEmpty(text)) {
             try {
@@ -21,11 +36,8 @@ public class MhDlaNotifierUtils {
                 digest.update(text.getBytes());
                 byte messageDigest[] = digest.digest();
 
-                // Create Hex String
-                StringBuffer hexString = new StringBuffer();
-                for (int i = 0; i < messageDigest.length; i++)
-                    hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
-                return hexString.toString();
+                String result = toHexadecimal(messageDigest);
+                return result;
 
             } catch (NoSuchAlgorithmException nsae) {
                 Log.e(TAG, "Algo MD5 non trouvé", nsae);
