4768959fd938670635e69fc8a5bb7e1bb0dfd63c
Arnaud Thimel <a.thimel@gmail.com>
2012-05-22 09:57:53 +0000

    Disable mock

--- src/org/zoumbox/mh_dla_notifier/Constants.java
+++ src/org/zoumbox/mh_dla_notifier/Constants.java
@@ -29,7 +29,7 @@ package org.zoumbox.mh_dla_notifier;
  */
 public class Constants {
 
-    public static final boolean mock = true;
+    public static final boolean mock = false;
 
     public static final String LOG_PREFIX = "MhDlaNotifier-";
     public static final String MH_PLAY_URL = "http://games.mountyhall.com/mountyhall/MH_Play/PlayStart.php";
