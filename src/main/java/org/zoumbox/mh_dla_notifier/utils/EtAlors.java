0d559b8d449be728156c99dba18aa60676016e90
Arnaud Thimel <thimel@codelutin.com>
2016-10-30 01:04:13 +0200

    Ajout des répliques

---
+++ src/main/java/org/zoumbox/mh_dla_notifier/utils/EtAlors.java
@@ -0,0 +1,45 @@
+package org.zoumbox.mh_dla_notifier.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EtAlors {
+    List<String> repliques = Arrays.asList(
+            "J'invite un tanche et je doigte un bonhomme de neige",
+            "Je bois du couscous et je nettoie un paysan",
+            "Je caresse un pompier et je pue du pull",
+            "Je déguise un poireau et je me marie avec Philippe Lavil",
+            "Je barbouille un anglais et je ressemble à Philippe Bouvard ",
+            "Je klaxonne au pressing et j'écoute Organiz",
+            "Je photocopie du sable et j'achète un hôtel",
+            "J'invente le lampadaire à voile et je déclare la guerre à la Suisse",
+            "Je crache sur le lama et je viole un pompier corse",
+            "J'amadoue Mariam et je jette Mamie dans les orties en case poulpe moldave",
+            "Je pêche un tire-fesse et je m'habille comme Tex",
+            "Je démonte une poule et j'envoie un fax à Noël Mamère",
+            "Je me met la main dans le fion et je cours derrière le chien",
+            "Je rabote un sans-papiers et je tête Sandy Valentino",
+            "Je mange une soutane et je tutoie David Charvet",
+            "Je mange mon slip et je roule en Safrane",
+            "Je vais à la bibliothèque et je m'auto-suce",
+            "J'épluche un château fort et je dépouille Francis Lalanne",
+            "Tu fais tomber la savonnette et je t'encule sous la douche",
+            "Je savonne un scout et je suce une famille esquimaux",
+            "C'est mignon mais un tout petit breton, tu perds une larme et crache sur la vitre",
+            "J'emmène un aveugle dans une maison close et retire les selles des vélos d'un couvent espagnol",
+            "Je promène un furet et je mange des glaces sans sucre",
+            "Je remboure frénétiquement une sardine au furilax et je cuis un pluviomètre à la moutarde",
+            "Je m'introduis chez Dark Vador et je mets du munster dans son casque",
+            "Je crache sur le R et j'embrasse Paul Amar",
+            "Je prend ta bagnole, je vais voir ta mère et je fouille dans le cul du chat",
+            "Je cambriole Monoprix et je danse la Salsa avec Joël Robuchon",
+            "Je contredis Winnie l'Ourson et je rhabille Jean-Luc Reichmann",
+            "Je mets un survêtement et j'apprends le kung fu à Lionel Jospin",
+            "Je me mets en short, je vais voir ta mère, et je fouille dans le cul du chat. (coup permis seulement en finale nationale)",
+            "Je badigeonne Christian Clavier d'huile solaire, et je compte jusqu'à cent",
+            "Bien joué, troubadour d'entrée",
+            "Je contre avec un pikachu (carte mystère)",
+            "Je prends trois de sauce et j'achète un squelette pour ma perruque",
+            "Je mets une moustache dans mon slip et je viole Cendrillon"
+    );
+}
